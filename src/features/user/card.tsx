efbeb3174d5b48427ef03e2856227044d4819e04
viktor <viktor@localhost>
2023-09-27 05:42:29 +0400

    feat: add user card component

---
+++ src/features/user/card.tsx
@@ -0,0 +1,41 @@
+import { mdiAccountCircle } from "@mdi/js";
+import Icon from "@mdi/react";
+import { DefaultSession } from "next-auth";
+
+export default function UserCard(
+    info: Exclude<DefaultSession["user"], undefined>
+) {
+    return (
+        <div className="flex gap-3 items-center p-2 border-[1px] border-solid rounded-md">
+            {typeof info.image === "string" && (
+                <div className="rounded-[50%] overflow-hidden">
+                    <ImageProxy
+                        src={info.image}
+                        alt="avatar"
+                        width={48}
+                        height={48}
+                    />
+                </div>
+            )}
+            <div>
+                <div>{info.name}</div>
+                <div className="text-gray-500">{info.email}</div>
+            </div>
+        </div>
+    );
+}
+
+function ImageProxy(props: {
+    src: string;
+    alt: string;
+    height: number;
+    width: number;
+}) {
+    if (props.src === "") {
+        // eslint-disable-next-line
+        return <Icon path={mdiAccountCircle} size={2} />;
+    }
+
+    // eslint-disable-next-line
+    return <img {...props} />;
+}
