3327768db1ddbc8a9ce0a938c9adf8c7f4d517a4
viktor <viktor@localhost>
2023-09-27 05:17:02 +0400

    feat: add loader component

---
+++ src/features/loading.tsx
@@ -0,0 +1,16 @@
+import Overlay from "@/shared/overlay";
+import { mdiLoading } from "@mdi/js";
+import Icon from "@mdi/react";
+
+export default function Loading() {
+    return (
+        <Overlay>
+            <div className="grid justify-center gap-3 px-8 py-4 rounded-md border-[1px] border-solid border-gray-200">
+                <div className="w-14 h-14 animate-spin mx-auto">
+                    <Icon path={mdiLoading} size={2.3} />
+                </div>
+                <div>Loading ...</div>
+            </div>
+        </Overlay>
+    );
+}
