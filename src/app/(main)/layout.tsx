d69c8f5dbe6fde3d2cde91bc23cca3132dd4e154
viktor <viktor@localhost>
2023-09-28 09:56:08 +0400

    style: set children height to the main layout, feat: add top bar to the main layout

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,4 +1,5 @@
-import BottomBar from "@/features/menu/bottom";
+import BottomBar from "@/features/bar/bottom";
+import TopBar from "@/features/bar/top";
 import PrivateServerComponent from "@/shared/private/server/component";
 
 export default function RootLayout({
@@ -9,8 +10,16 @@ export default function RootLayout({
     return (
         <div className="min-h-screen w-full flex items-center justify-center">
             <div className="w-[360px] h-screen max-h-[800px] border-[1em] border-solid border-[#d8eaff] rounded-2xl relative">
-                {children}
+                <div
+                    className="my-12 h-full box-border overflow-auto"
+                    style={{
+                        maxHeight: "calc(100% - 6rem)",
+                    }}
+                >
+                    {children}
+                </div>
                 <PrivateServerComponent>
+                    <TopBar />
                     <BottomBar />
                 </PrivateServerComponent>
             </div>
