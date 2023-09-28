0cd31209d5011c6ed6497a62fc7753419a9cbe59
viktor <viktor@localhost>
2023-09-28 06:31:09 +0400

    refactor: wrap bottom bar and side menu with private component, remove session checking from the bottom bar

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,5 +1,6 @@
 import BottomBar from "@/features/menu/bottom";
 import SideMenu from "@/features/menu/side";
+import Private from "@/shared/private";
 
 export default function RootLayout({
     children,
@@ -10,8 +11,10 @@ export default function RootLayout({
         <div className="min-h-screen w-full flex items-center justify-center">
             <div className="w-[360px] h-screen max-h-[800px] border-[1em] border-solid border-[#d8eaff] rounded-2xl relative">
                 {children}
-                {/* <SideMenu /> */}
-                <BottomBar />
+                <Private>
+                    <SideMenu />
+                    <BottomBar />
+                </Private>
             </div>
         </div>
     );
