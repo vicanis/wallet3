aea6acd14ec27ca3b5089d2279c17e67e9618d88
viktor <viktor@localhost>
2023-09-28 09:17:04 +0400

    feat: remove side menu from the main layout, remove old side menu and private component wrapper

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,6 +1,5 @@
 import BottomBar from "@/features/menu/bottom";
-import SideMenu from "@/features/menu/side";
-import Private from "@/shared/private";
+import PrivateServerComponent from "@/shared/private/server/component";
 
 export default function RootLayout({
     children,
@@ -11,10 +10,9 @@ export default function RootLayout({
         <div className="min-h-screen w-full flex items-center justify-center">
             <div className="w-[360px] h-screen max-h-[800px] border-[1em] border-solid border-[#d8eaff] rounded-2xl relative">
                 {children}
-                <Private>
-                    <SideMenu />
+                <PrivateServerComponent>
                     <BottomBar />
-                </Private>
+                </PrivateServerComponent>
             </div>
         </div>
     );
