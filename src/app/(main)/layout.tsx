13ad9fe43e7d1c048148231096101cc249d205a4
viktor <viktor@localhost>
2023-09-28 06:20:02 +0400

    fix: remove wrapping with <div> of the children in the main layout, hide side menu

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -9,8 +9,8 @@ export default function RootLayout({
     return (
         <div className="min-h-screen w-full flex items-center justify-center">
             <div className="w-[360px] h-screen max-h-[800px] border-[1em] border-solid border-[#d8eaff] rounded-2xl relative">
-                <div>{children}</div>
-                <SideMenu />
+                {children}
+                {/* <SideMenu /> */}
                 <BottomBar />
             </div>
         </div>
